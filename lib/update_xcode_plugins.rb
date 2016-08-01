1377e39c16573c9ca100363070bf1e4da0d29a8d
Mahdi Bchetnia <mahdi@outlook.com>
2016-08-01 15:04:09 +0900

    Don't require dependencies when ran from launch agent
    Trying to setup the correct ruby environment for each system (system, rvm, rbenv) while running from a launch agent is a pain in the ass. Rather not using dependencies and call the gem with the system ruby.

--- lib/update_xcode_plugins.rb
+++ lib/update_xcode_plugins.rb
@@ -1,7 +1,8 @@
 require 'English'
 require 'fileutils'
-require 'colorize'
-require 'inquirer'
+require_relative 'cli'
+require 'colorize' unless CLI.no_colors?
+require 'inquirer' unless CLI.non_interactive?
 require_relative 'version'
 require_relative 'plugins_updater'
 require_relative 'xcode_unsigner'
